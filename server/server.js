71b77bc8779385c824b8e1be71794812f59b094d
Quan Nham <quannham6@gmail.com>
2018-08-09 15:44:50 -0400

    socket setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const http = require('http');   //express is actually built on http
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
-const express = require('express');
 const app = express();
+
+const server = http.createServer(app);   //this method is basically called with the app.listen method
+const io = socketIO(server);
 
 app.use(express.static(publicPath));  //for the app to access static assets in the public folder
 
-const port = process.env.PORT || 3000;
+io.on('connection', (socket) => {   //our event listener
+	console.log('new user connected');
 
-app.listen(port, () => {
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	})
+})
+
+
+
+server.listen(port, () => {
 	console.log(`starting on port ${port}`);
 });

da2d013cbe6018066f32ea2c3070ba3befc1becd
Quan Nham <quannham6@gmail.com>
2018-08-09 21:23:31 -0400

    emit message on createMessage

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();  //making a request to the server to open up a web socket
+
+socket.on('connect', function() { 
+	console.log('connected to server');
+	//we are emitting this event from the client to server -- irl would be the filling out of a form
+	// socket.emit('createMessage', {
+	// 	from: 'meeshy@example.com',
+	// 	text: 'hey!'
+	// });
+});
+
+socket.on('disconnect', function() {
+	console.log('disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+	console.log('new message received!', message);
+});
--- server/server.js
+++ server/server.js
@@ -15,10 +15,25 @@ app.use(express.static(publicPath));  //for the app to access static assets in t
 
 io.on('connection', (socket) => {   //our event listener
 	console.log('new user connected');
+ 
+	// socket.emit('newMessage', {     //emit will send the 2nd arg to the listener on our client side and pass that in as the arg to the callback func
+	// 	from: 'quanny@example.com',
+	// 	text: 'Hey whats up thotty',
+	// 	createdAt: 123
+	// }); 
+
+	socket.on('createMessage', (message) => {
+		console.log('logging message', message);
+		io.emit('newMessage', {       //emitting an event to every single socket/connection
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		}) 
+	});
 
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
-	})
+	});
 })
 
 
